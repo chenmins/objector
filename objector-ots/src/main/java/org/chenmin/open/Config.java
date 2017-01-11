646dac297fa4761baee398dd9a2bf735f27c33b6
Chenmin <Chenmin@ChenminServer>
2017-01-11 22:27:04 +0800

    add config

---
+++ objector-ots/src/main/java/org/chenmin/open/Config.java
@@ -0,0 +1,32 @@
+package org.chenmin.open;
+
+import org.apache.commons.configuration.CompositeConfiguration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+
+public class Config {
+
+	public static String get(String key) {
+		CompositeConfiguration config = init();
+		return config.getString(key);
+	}
+	
+	private static CompositeConfiguration config = null;
+
+	private static CompositeConfiguration init() {
+		if(config != null)
+			return config;
+		CompositeConfiguration config = new CompositeConfiguration();
+		// config.addConfiguration(new SystemConfiguration());
+		try {
+			config.addConfiguration(new PropertiesConfiguration("objector.properties"));
+		} catch (ConfigurationException e) {
+			e.printStackTrace();
+		}
+		return config;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(get("ALIYUN_ACCESS_KEY"));
+	}
+}
