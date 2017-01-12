654461c6e8d3be45bf26aec0fc192b43b4f22c01
Chenmin <Chenmin@ChenminServer>
2017-01-12 20:12:07 +0800

    JDK 1.6 complie

--- objector/src/main/java/org/chenmin/open/objector/StoreException.java
+++ objector/src/main/java/org/chenmin/open/objector/StoreException.java
@@ -19,10 +19,6 @@ public class StoreException extends Exception {
 		super();
 	}
 
-	public StoreException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
-		super(arg0, arg1, arg2, arg3);
-	}
-
 	public StoreException(String arg0, Throwable arg1) {
 		super(arg0, arg1);
 	}
