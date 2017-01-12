e971ed79a2cb5b80ac8b9884566aef8e5402c191
Chenmin <Chenmin@ChenminServer>
2017-01-12 22:58:17 +0800

    默认实现类

---
+++ objector-ots/src/main/java/org/chenmin/open/objector/StoreTableRow.java
@@ -0,0 +1,44 @@
+package org.chenmin.open.objector;
+
+import java.util.List;
+import java.util.Map;
+
+public class StoreTableRow implements IStoreTableRow {
+
+	@Override
+	public String getTablename() {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+	@Override
+	public List<PrimaryKeySchemaObject> getPrimaryKey() {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+	@Override
+	public Map<String, PrimaryKeyValueObject> getPrimaryKeyValue() {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+	@Override
+	public Map<String, ColumnValueObject> getColumnValue() {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+	@Override
+	public void setPrimaryKeyValue(Map<String, PrimaryKeyValueObject> v) {
+		// TODO 自动生成的方法存根
+
+	}
+
+	@Override
+	public void setColumnValue(Map<String, ColumnValueObject> v) {
+		// TODO 自动生成的方法存根
+
+	}
+
+}
