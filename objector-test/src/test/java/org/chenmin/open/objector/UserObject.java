400a6f0b97fc39f25969f6a5e42af5fce5d12f67
chenmin <admin@chenmin.org>
2019-01-03 11:48:02 +0800

    增加版本 测试

--- objector-test/src/test/java/org/chenmin/open/objector/UserObject.java
+++ objector-test/src/test/java/org/chenmin/open/objector/UserObject.java
@@ -2,11 +2,15 @@ package org.chenmin.open.objector;
 
 import java.io.Serializable;
 
+import org.chenmin.open.objector.annotation.CapacityUnit;
 import org.chenmin.open.objector.annotation.Column;
 import org.chenmin.open.objector.annotation.Entity;
+import org.chenmin.open.objector.annotation.EntityOption;
 import org.chenmin.open.objector.annotation.Key;
 
-@Entity
+@Entity("USR24")
+@EntityOption(maxVersions=2)
+@CapacityUnit(readCapacityUnit=0,writeCapacityUnit=0)
 public class UserObject implements Serializable {
 
 	/**
