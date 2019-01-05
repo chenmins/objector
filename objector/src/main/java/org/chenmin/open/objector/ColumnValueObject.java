3cf81ebe26af73651bf2d54bfdbf8d7241f036a4
chenmin <admin@chenmin.org>
2019-01-05 19:02:09 +0800

    增加数据转换方法

--- objector/src/main/java/org/chenmin/open/objector/ColumnValueObject.java
+++ objector/src/main/java/org/chenmin/open/objector/ColumnValueObject.java
@@ -15,8 +15,72 @@ public class ColumnValueObject {
 		return value;
 	}
 
-	public void setValue(Object value) {
-		this.value = value;
+	// STRING, INTEGER, BOOLEAN, DOUBLE, BINARY;
+	public String asString() {
+		if (this.type != ColumnTypeObject.STRING) {
+			throw new IllegalStateException("The type of column is not STRING.");
+		}
+		return (String) this.value;
+	}
+
+	public long asLong() {
+		if (this.type != ColumnTypeObject.INTEGER) {
+			throw new IllegalStateException("The type of column is not INTEGER.");
+		}
+		return ((Long) this.value).longValue();
+	}
+
+	public int asInt() {
+		if (this.type != ColumnTypeObject.INTEGER) {
+			throw new IllegalStateException("The type of column is not INTEGER.");
+		}
+		return ((Long) this.value).intValue();
+	}
+
+	public double asDouble() {
+		if (this.type != ColumnTypeObject.DOUBLE) {
+			throw new IllegalStateException("The type of column is not DOUBLE.");
+		}
+		return ((Double) this.value).doubleValue();
+	}
+
+	public boolean asBoolean() {
+		if (this.type != ColumnTypeObject.BOOLEAN) {
+			throw new IllegalStateException("The type of column is not BOOLEAN.");
+		}
+		return ((Boolean) this.value).booleanValue();
+	}
+
+	public byte[] asBinary() {
+		if (this.type != ColumnTypeObject.BINARY) {
+			throw new IllegalStateException("The type of column is not BINARY.");
+		}
+		return (byte[]) this.value;
+	}
+
+	public static ColumnValueObject fromString(String value) {
+		return new ColumnValueObject(value, ColumnTypeObject.STRING);
+	}
+
+	public static ColumnValueObject fromLong(long value) {
+		return new ColumnValueObject(Long.valueOf(value), ColumnTypeObject.INTEGER);
+	}
+
+	public static ColumnValueObject fromInt(int value) {
+		return new ColumnValueObject(Long.valueOf(value), ColumnTypeObject.INTEGER);
+	}
+
+	
+	public static ColumnValueObject fromBinary(byte[] value) {
+		return new ColumnValueObject(value, ColumnTypeObject.BINARY);
+	}
+
+	public static ColumnValueObject fromDouble(double value) {
+		return new ColumnValueObject(Double.valueOf(value), ColumnTypeObject.DOUBLE);
+	}
+
+	public static ColumnValueObject fromBoolean(boolean value) {
+		return new ColumnValueObject(Boolean.valueOf(value), ColumnTypeObject.BOOLEAN);
 	}
 
 	public byte[] getRawData() {
--- objector/src/main/java/org/chenmin/open/objector/PrimaryKeyValueObject.java
+++ objector/src/main/java/org/chenmin/open/objector/PrimaryKeyValueObject.java
@@ -11,8 +11,52 @@ public class PrimaryKeyValueObject {
 		this.type = type;
 	}
 
+	public static PrimaryKeyValueObject fromString(String value) {
+		return new PrimaryKeyValueObject(value, PrimaryKeyTypeObject.STRING);
+	}
+
+	public static PrimaryKeyValueObject fromLong(long value) {
+		return new PrimaryKeyValueObject(Long.valueOf(value), PrimaryKeyTypeObject.INTEGER);
+	}
+
+	public static PrimaryKeyValueObject fromInt(int value) {
+		return new PrimaryKeyValueObject(Long.valueOf(value), PrimaryKeyTypeObject.INTEGER);
+	}
+
+	public static PrimaryKeyValueObject fromBinary(byte[] value) {
+		return new PrimaryKeyValueObject(value, PrimaryKeyTypeObject.BINARY);
+	}
+
 	public Object getValue() {
 		return value;
+	}
+
+	public String asString() {
+		if (this.type != PrimaryKeyTypeObject.STRING) {
+			throw new IllegalStateException("The type of primary key is not STRING.");
+		}
+		return (String) this.value;
+	}
+
+	public long asLong() {
+		if (this.type != PrimaryKeyTypeObject.INTEGER) {
+			throw new IllegalStateException("The type of primary key is not INTEGER.");
+		}
+		return ((Long) this.value).longValue();
+	}
+
+	public int asInt() {
+		if (this.type != PrimaryKeyTypeObject.INTEGER) {
+			throw new IllegalStateException("The type of primary key is not INTEGER.");
+		}
+		return ((Long) this.value).intValue();
+	}
+
+	public byte[] asBinary() {
+		if (this.type != PrimaryKeyTypeObject.BINARY) {
+			throw new IllegalStateException("The type of primary key is not BINARY");
+		}
+		return (byte[]) this.value;
 	}
 
 	public void setValue(Object value) {
