a0f531c543a54a2ab8142ccbbee547b1d6cebeb3
chenmin <admin@chenmin.org>
2019-01-05 21:11:48 +0800

    测试自增 保存和查询，修改，删除

--- objector-test/src/test/java/org/chenmin/open/objector/test/TestUserBlog.java
+++ objector-test/src/test/java/org/chenmin/open/objector/test/TestUserBlog.java
@@ -1,5 +1,10 @@
 package org.chenmin.open.objector.test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import org.chenmin.open.objector.IStore;
 import org.chenmin.open.objector.StoreException;
 import org.chenmin.open.objector.StoreFactory;
@@ -7,8 +12,6 @@ import org.chenmin.open.objector.UserBlog;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class TestUserBlog {
 	private static IStore store;
@@ -31,8 +34,8 @@ public class TestUserBlog {
 	 */
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
-//		UserBlog u = new UserBlog();
-//		store.deleteTable(u);
+		UserBlog u = new UserBlog();
+		store.deleteTable(u);
 	}
 	@Test
 	public void test() throws StoreException {
@@ -46,5 +49,25 @@ public class TestUserBlog {
 //		u.setReadCount(0);
 //		System.out.println("::"+u.getId());
 		assertTrue(store.save(u));
+//		System.out.println("::"+u.getId());
+		UserBlog u1 = new UserBlog();
+		u1.setOpenid(u.getOpenid());
+		u1.setId(u.getId());
+		assertTrue(store.get(u1));
+		assertNotNull(u1.getBody());
+		u1.setTitle(title);
+		u1.setReadCount(2);
+		assertTrue(store.update(u1));
+		u1 = new UserBlog();
+		u1.setOpenid(openid);
+		u1.setId(u.getId());
+		assertTrue(store.get(u1));
+		assertEquals(u1.getTitle(), title);
+		u1 = new UserBlog();
+		u1.setOpenid(openid);
+		u1.setId(u.getId());
+		assertTrue(store.del(u1));
+		assertFalse(store.get(u1));
+//		assertNull(u1.getTitle(),null);
 	}
 }
